b63dc73a8f610ee55c372677425d75cdc7d6f4d0
Edgard Levy <edgardlevy@gmail.com>
2020-05-11 23:47:12 -0300

    - added validation of user credentials

--- backend/src/routes/users.router.ts
+++ backend/src/routes/users.router.ts
@@ -5,13 +5,17 @@ import CreateUserService from '../services/CreateUserService';
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
+  try {
+    const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-  const user = await createUser.execute({ name, email, password });
+    const user = await createUser.execute({ name, email, password });
 
-  return response.json(user);
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default usersRouter;
