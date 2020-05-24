dd821067349d7d87927db0784d0dd4d28cdfc1c0
Edgard Levy <edgardlevy@gmail.com>
2020-05-23 23:38:44 -0300

    import external fonts

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
