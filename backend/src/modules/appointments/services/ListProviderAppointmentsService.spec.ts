ed993b0cf5d823b6bcc7775ed8edcb088b5e839f
Edgard Levy <edgardlevy@gmail.com>
2020-06-10 23:14:53 -0300

    created ProviderAppointmentsController

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+  it('should be able to show day appointments from the provider', async () => {
+    // 0 january
+
+    const appointments1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointments2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const availability = await listProviderAppointmentsService.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(availability).toEqual([appointments1, appointments2]);
+  });
+});
