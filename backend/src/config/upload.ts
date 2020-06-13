6d2b21abb8adcf8dfc8bbe6795492bacffe55598
Edgard Levy <edgardlevy@gmail.com>
2020-06-13 10:17:19 -0300

    applying storage provider

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,18 +1,34 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+export interface IUploadConfig {
+  driver: 'disk' | 'S3';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: { storage: StorageEngine };
+  config: {
+    disk: {};
+  };
+}
+
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
+  driver: process.env.STORAGE_DRIVER || 'disk',
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
-    },
-  }),
-};
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+  },
+} as IUploadConfig;
---
+++ backend/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,37 @@
+import aws, { S3 } from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+export default class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: process.env.AWS_DEFAULT_REGION,
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+    const fileContent = await fs.promises.readFile(originalPath, {
+      encoding: 'utf-8',
+    });
+    await this.client
+      .putObject({
+        Bucket: 'app-gobarber',
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+      })
+      .promise();
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({ Bucket: 'app-gobarber', Key: file })
+      .promise();
+  }
+}
